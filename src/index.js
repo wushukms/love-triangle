c87d52ec1a802d657049fe02ef3d52d01a997081
wushukms <safina1999@rambler.ru>
2018-09-11 23:08:23 +0300

    secondtask

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let counter;
+    counter = 0;
+    for (i=0; i<preferences.length; i++) {
+        if ((i+1) != preferences[i]){
+            if (i == preferences[preferences[preferences[i]-1]-1]-1) {
+                counter+=1;
+            }
+        }
+    }
+    return counter/3;
+    // your implementation
 };
